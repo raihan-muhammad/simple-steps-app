25b3b456e61ebcd71f32358ac56fc6659125c4e2
raihan-muhammad <sukasukangoding@gmail.com>
2024-01-10 21:48:59 +0700

    feat: add messages for each steps

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
-  return <div className="steps">Hello There!</div>;
+  const step = 1;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button>Previous</button>
+        <button>Next</button>
+      </div>
+    </div>
+  );
 }
